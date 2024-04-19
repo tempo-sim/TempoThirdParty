18f2cf329eacf7f548933c0b9838afe9a10f80d3
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-04-18 21:51:01 -0400

    Fix Symbol Visibility in All Libraries

--- Script/build_windows.sh
+++ Script/build_windows.sh
@@ -51,6 +51,9 @@ echo -e "Using Unreal Engine ThirdParty: $UE_THIRD_PARTY_PATH\n";
 
 echo -e "All prerequisites satisfied. Starting build.\n"
 
+NUM_JOBS="$(nproc --all)"
+echo -e "Detected $NUM_JOBS processors. Will use $NUM_JOBS jobs.\n"
+
 echo "Removing contents of Output and Build folders"
 find "$ROOT_DIR/Output" -maxdepth 1 -mindepth 1 -type d -exec rm -rf {} \;
 find "$ROOT_DIR/Build" -maxdepth 1 -mindepth 1 -type d -exec rm -rf {} \;
@@ -60,11 +63,12 @@ cd "$ROOT_DIR/Source/re2"
 git reset --hard && git apply "$ROOT_DIR/Patch/re2.patch"
 mkdir -p "$ROOT_DIR/Build/Windows/re2" && cd "$ROOT_DIR/Build/Windows/re2"
 cmake -G "Visual Studio 17 2022" \
- -DCMAKE_INSTALL_PREFIX="$ROOT_DIR/Output/RE2" \
- -DCMAKE_INSTALL_LIBDIR="Libraries/Windows" \
- -DCMAKE_INSTALL_CMAKEDIR="Libraries/Windows/cmake" \
+ -DCMAKE_INSTALL_PREFIX="$ROOT_DIR/Output/gRPC" \
+ -DCMAKE_INSTALL_BINDIR="Binaries/Windows" -DCMAKE_INSTALL_LIBDIR="Libraries/Windows/RE2" -DCMAKE_INSTALL_INCLUDEDIR="Includes" -DCMAKE_INSTALL_CMAKEDIR="Libraries/cmake" \
+ -DCMAKE_CXX_FLAGS=" /EHsc /MP$NUM_JOBS /std:c++17 /permissive " \
+ -DCMAKE_CXX_EXTENSIONS=OFF -DCMAKE_CXX_STANDARD=17 \
  "$ROOT_DIR/Source/re2"
-cmake --build . --target install --config Release --parallel
+cmake --build . --target install --config Release -j "$NUM_JOBS" -- -m:"$NUM_JOBS"
 echo -e "Successfully built re2.\n"
 
 echo -e "Building abseil-cpp..."
@@ -72,12 +76,13 @@ cd "$ROOT_DIR/Source/abseil-cpp"
 git reset --hard && git apply "$ROOT_DIR/Patch/abseil-cpp.patch"
 mkdir -p "$ROOT_DIR/Build/Windows/abseil-cpp" && cd "$ROOT_DIR/Build/Windows/abseil-cpp"
 cmake -G "Visual Studio 17 2022" \
- -DCMAKE_INSTALL_PREFIX="$ROOT_DIR/Output/Abseil" \
- -DCMAKE_INSTALL_LIBDIR="Libraries/Windows" \
- -DCMAKE_INSTALL_CMAKEDIR="Libraries/Windows/cmake" -DCMAKE_CXX_STANDARD=17 \
+ -DCMAKE_INSTALL_PREFIX="$ROOT_DIR/Output/gRPC" \
+ -DCMAKE_INSTALL_BINDIR="Binaries/Windows" -DCMAKE_INSTALL_LIBDIR="Libraries/Windows/Abseil" -DCMAKE_INSTALL_INCLUDEDIR="Includes" -DCMAKE_INSTALL_CMAKEDIR="Libraries/cmake" \
+ -DCMAKE_CXX_FLAGS=" -D ABSL_BUILD_DLL=1 /EHsc /MP$NUM_JOBS /std:c++17 /permissive " \
+ -DCMAKE_CXX_EXTENSIONS=OFF -DCMAKE_CXX_STANDARD=17 \
  -DBUILD_TESTING=False -DABSL_PROPAGATE_CXX_STD=True \
  "$ROOT_DIR/Source/abseil-cpp"
-cmake --build . --target INSTALL --config Release --parallel
+cmake --build . --target INSTALL --config Release -j "$NUM_JOBS"
 echo -e "Successfully built abseil-cpp.\n"
 
 echo -e "Building protobuf..."
@@ -85,16 +90,18 @@ cd "$ROOT_DIR/Source/protobuf"
 git reset --hard && git apply "$ROOT_DIR/Patch/protobuf.patch"
 mkdir -p "$ROOT_DIR/Build/Windows/protobuf" && cd "$ROOT_DIR/Build/Windows/protobuf"
 cmake -G "Visual Studio 17 2022" \
- -DCMAKE_INSTALL_PREFIX="$ROOT_DIR/Output/Protobuf" \
+ -DCMAKE_INSTALL_PREFIX="$ROOT_DIR/Output/gRPC" \
+ -DCMAKE_INSTALL_BINDIR="Binaries/Windows" -DCMAKE_INSTALL_LIBDIR="Libraries/Windows/Protobuf" -DCMAKE_INSTALL_INCLUDEDIR="Includes" -DCMAKE_INSTALL_CMAKEDIR="Libraries/cmake" \
+ -DCMAKE_CXX_FLAGS=" -D PROTOBUF_USE_DLLS=1 -D LIBPROTOBUF_EXPORTS=1 /EHsc /MP$NUM_JOBS /std:c++17 /permissive " \
+ -DCMAKE_CXX_EXTENSIONS=OFF -DCMAKE_CXX_STANDARD=17 \
  -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded$<$<CONFIG:Debug>:Debug>DLL" \
  -Dprotobuf_BUILD_TESTS=false -Dprotobuf_WITH_ZLIB=false \
+ -Dprotobuf_BUILD_PROTOC_BINARIES=ON -Dprotobuf_BUILD_LIBPROTOC=ON \
  -Dprotobuf_DEBUG_POSTFIX="" \
- -DCMAKE_INSTALL_LIBDIR="Libraries/Windows" \
- -DCMAKE_INSTALL_CMAKEDIR="Libraries/Windows/cmake" \
  -Dprotobuf_MSVC_STATIC_RUNTIME=false \
- -Dprotobuf_ABSL_PROVIDER=package -Dabsl_DIR="$ROOT_DIR/Output/Abseil/Libraries/Windows/cmake" \
+ -Dprotobuf_ABSL_PROVIDER=package -Dabsl_DIR="$ROOT_DIR/Output/gRPC/Libraries/cmake" \
  "$ROOT_DIR/Source/protobuf"
-cmake --build . --target INSTALL --config Release --parallel
+cmake --build . --target INSTALL --config Release -j "$NUM_JOBS"
 echo -e "Successfully built protobuf.\n"
 
 echo -e "Building gRPC..."
@@ -103,12 +110,13 @@ git reset --hard && git apply "$ROOT_DIR/Patch/gRPC.patch"
 mkdir -p "$ROOT_DIR/Build/Windows/gRPC" && cd "$ROOT_DIR/Build/Windows/gRPC"
 cmake -G "Visual Studio 17 2022" \
  -DCMAKE_INSTALL_PREFIX="$ROOT_DIR/Output/gRPC" \
- -DgRPC_INSTALL_LIBDIR="Libraries/Windows" \
- -DgRPC_INSTALL_CMAKEDIR="Libraries/Windows/cmake" -DgRPC_USE_CARES=OFF -DCMAKE_CXX_STANDARD=17 \
- -DgRPC_ABSL_PROVIDER=package -Dabsl_DIR="$ROOT_DIR/Output/Abseil/Libraries/Windows/cmake" \
- -DgRPC_RE2_PROVIDER=package -Dre2_DIR="$ROOT_DIR/Output/RE2/Libraries/Windows/cmake" \
- -DgRPC_PROTOBUF_PROVIDER=package -DProtobuf_DIR="$ROOT_DIR/Output/Protobuf/Libraries/Windows/cmake" \
- -Dutf8_range_DIR="$ROOT_DIR/Output/Protobuf/Libraries/Windows/cmake" \
+ -DgRPC_INSTALL_BINDIR="Binaries/Windows" -DgRPC_INSTALL_LIBDIR="Libraries/Windows/gRPC" -DgRPC_INSTALL_INCLUDEDIR="Includes" -DgRPC_INSTALL_CMAKEDIR="Libraries/cmake" -DgRPC_INSTALL_SHAREDIR="Libraries/share" \
+ -DCMAKE_CXX_FLAGS=" -D GRPC_DLL_EXPORTS=1 -D GRPCXX_DLL_EXPORTS=1 -D GPR_DLL_EXPORTS=1 /EHsc /MP$NUM_JOBS /std:c++17 /permissive" \
+ -DgRPC_USE_CARES=OFF -DCMAKE_CXX_EXTENSIONS=OFF -DCMAKE_CXX_STANDARD=17 \
+ -DgRPC_ABSL_PROVIDER=package -Dabsl_DIR="$ROOT_DIR/Output/gRPC/Libraries/cmake" \
+ -DgRPC_RE2_PROVIDER=package -Dre2_DIR="$ROOT_DIR/Output/gRPC/Libraries/cmake" \
+ -DgRPC_PROTOBUF_PROVIDER=package -DProtobuf_DIR="$ROOT_DIR/Output/gRPC/Libraries/cmake" \
+ -Dutf8_range_DIR="$ROOT_DIR/Output/gRPC/Libraries/cmake" \
  -DgRPC_ZLIB_PROVIDER=package \
  -DZLIB_INCLUDE_DIR="$UE_THIRD_PARTY_PATH/zlib/1.2.13/include" \
  -DZLIB_LIBRARY_RELEASE="$UE_THIRD_PARTY_PATH/zlib/1.2.13/lib/Win64/Release/zlibstatic.lib" \
@@ -128,29 +136,28 @@ cmake -G "Visual Studio 17 2022" \
  -DgRPC_BUILD_GRPC_NODE_PLUGIN=OFF -DgRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN=OFF \
  -DgRPC_BUILD_GRPC_PHP_PLUGIN=OFF -DgRPC_BUILD_GRPC_PYTHON_PLUGIN=ON \
  -DgRPC_BUILD_GRPC_RUBY_PLUGIN=OFF \
- -DPACKAGE_PREFIX_DIR="$ROOT_DIR/Output/RE2" \
+ -DPACKAGE_PREFIX_DIR="$ROOT_DIR/Output/gRPC" \
  "$ROOT_DIR/Source/gRPC"
-cmake --build . --target INSTALL --config Release --parallel
+cmake --build . --target INSTALL --config Release -j "$NUM_JOBS"
 echo -e "Successfully built gRPC.\n"
 
 echo -e "Cleaning up Output directory...\n"
-mv "$ROOT_DIR/Output/RE2/include" "$ROOT_DIR/Output/RE2/Includes"
-rm -rf "$ROOT_DIR/Output/RE2/Libraries/Windows/cmake"
-mv "$ROOT_DIR/Output/Abseil/include" "$ROOT_DIR/Output/Abseil/Includes"
-rm -rf "$ROOT_DIR/Output/Abseil/Libraries/Windows/cmake"
-mv "$ROOT_DIR/Output/Protobuf/include" "$ROOT_DIR/Output/Protobuf/Includes"
-mv "$ROOT_DIR/Output/Protobuf/bin" "$ROOT_DIR/Output/Protobuf/Binaries"
-rm -rf "$ROOT_DIR/Output/Protobuf/Libraries/Windows/cmake"
-rm -rf "$ROOT_DIR/Output/Protobuf/Libraries/Windows/pkgconfig"
-mv "$ROOT_DIR/Output/gRPC/include" "$ROOT_DIR/Output/gRPC/Includes"
-mv "$ROOT_DIR/Output/gRPC/bin" "$ROOT_DIR/Output/gRPC/Binaries"
-rm -rf "$ROOT_DIR/Output/gRPC/Libraries/Windows/cmake"
-rm -rf "$ROOT_DIR/Output/gRPC/share"
-rm -rf "$ROOT_DIR/Output/gRPC/Libraries/Windows/pkgconfig"
+rm -rf "$ROOT_DIR/Output/gRPC/Libraries/cmake"
+rm -rf "$ROOT_DIR/Output/gRPC/Libraries/share"
+rm -rf "$ROOT_DIR/Output/gRPC/Libraries/Windows/RE2/pkgconfig"
+rm -rf "$ROOT_DIR/Output/gRPC/Libraries/Windows/Abseil/pkgconfig"
+rm -rf "$ROOT_DIR/Output/gRPC/Libraries/Windows/Protobuf/pkgconfig"
+rm -rf "$ROOT_DIR/Output/gRPC/Libraries/Windows/gRPC/pkgconfig"
+
+echo -e "Removing unused libraries...\n"
+rm -f "$ROOT_DIR/Output/gRPC/Libraries/Windows/gRPC/grpc++_unsecure.lib" # We use libgrpc++.lib
+rm -f "$ROOT_DIR/Output/gRPC/Libraries/Windows/gRPC/grpc_unsecure.lib" # We use libgrpc.lib
+rm -f "$ROOT_DIR/Output/gRPC/Libraries/Windows/Protobuf/protobuf-lite.lib" # We use libprotobuf.lib
+rm -f "$ROOT_DIR/Output/gRPC/Libraries/Windows/Protobuf/protoc.lib" # Only needed during build of grpc code gen plugins
 
 echo -e "Archiving outputs...\n"
 ARCHIVE="$ROOT_DIR/Release/TempoThirdParty-Windows-$TAG.tar.gz"
 rm -rf "$ARCHIVE"
-tar -C "$ROOT_DIR/Output" -czf "$ARCHIVE" Abseil gRPC Protobuf RE2
+tar -C "$ROOT_DIR/Output" -czf "$ARCHIVE" gRPC
 
 echo "Done! Archive: $ARCHIVE"
