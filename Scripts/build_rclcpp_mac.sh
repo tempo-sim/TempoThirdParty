9347ccfe455bc9fbc2bd671342f3cdc174c1db55
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-06-20 18:36:08 -0400

    Check in Script

--- Scripts/build_rclcpp_mac.sh
+++ Scripts/build_rclcpp_mac.sh
@@ -48,6 +48,18 @@ if ! brew ls asio; then
     exit 1
 fi
 
+# Check for boost-python3
+if ! brew ls boost-python3; then
+    echo "Couldn't find boost-python3. Please install (brew install boost-python3)."
+    exit 1
+fi
+
+# Check for theora
+if ! brew ls theora; then
+    echo "Couldn't find theora. Please install (brew install theora)."
+    exit 1
+fi
+
 # Check for tag
 TAG=$(git name-rev --tags --name-only "$(git rev-parse HEAD)")
 if [ "$TAG" = "undefined" ]; then
@@ -64,7 +76,8 @@ echo -e "Detected $NUM_JOBS processors. Will use $NUM_JOBS jobs.\n"
 
 echo -e "Removing stale Outputs and Builds\n"
 rm -rf "$ROOT_DIR/Outputs/rclcpp"
-rm -rf "$ROOT_DIR/Builds/rclcpp"
+#rm -rf "$ROOT_DIR/Builds/rclcpp"
+#rm -rf "$ROOT_DIR/Source/rclcpp/install"
 
 echo -e "Creating Python virtual environment for build.\n"
 python3 -m venv "$ROOT_DIR/Builds/rclcpp/venv"
@@ -73,32 +86,56 @@ source "$ROOT_DIR/Builds/rclcpp/venv/bin/activate"
 pip install colcon-common-extensions
 pip install lark==1.1.1
 pip install numpy
+pip install netifaces
 
 echo "Applying Tempo patches..."
+cd "$ROOT_DIR/Source/rclcpp"
+find . -maxdepth 1 -type d -exec sh -c 'cd $0 && git reset --hard' {} \;
 cd "$ROOT_DIR/Source/rclcpp/rcpputils"
-git reset --hard && git apply "$ROOT_DIR/Patches/rcpputils.patch"
+git apply "$ROOT_DIR/Patches/rcpputils.patch"
+cd "$ROOT_DIR/Source/rclcpp/rclcpp"
+git apply "$ROOT_DIR/Patches/rclcpp.patch"
+cd "$ROOT_DIR/Source/rclcpp/rmw"
+git apply "$ROOT_DIR/Patches/rmw.patch"
 
 echo "Building rclcpp..."
 mkdir -p "$ROOT_DIR/Builds/rclcpp/Mac"
 cd "$ROOT_DIR/Source/rclcpp"
 
- #--install-base "$ROOT_DIR/Outputs/rclcpp/Libraries/Mac" 
-colcon build --packages-skip-by-dep python_qt_binding \
- --build-base "$ROOT_DIR/Builds/rclcpp/Mac" \
- --ament-cmake-args \
- -DCMAKE_POLICY_DEFAULT_CMP0148=OLD \
- -DCMAKE_CXX_FLAGS="-Wno-error=strict-prototypes -Wno-error=deprecated-copy" --no-warn-unused-cli
-
 mkdir -p "$ROOT_DIR/Outputs/rclcpp/Libraries/Mac"
 mkdir -p "$ROOT_DIR/Outputs/rclcpp/Includes"
 
-find "$ROOT_DIR/Source/rclcpp/install" -type f -name "*.dylib" -exec cp {} "$ROOT_DIR/Outputs/rclcpp/Libraries/Mac" \;
-find "$ROOT_DIR/Source/rclcpp/install" -type d -maxdepth 1 -exec bash -c '[[ -e "$1/include/$(basename $1)" ]] && cp -r "$1/include/$(basename $1)" "$2/Outputs/rclcpp/Includes"' _ {} "$ROOT_DIR" \;
-#INCLUDE_DIRS=$(find "$ROOT_DIR/Source/rclcpp/install" -type d -name include)
-#for INCLUDE_DIR in $INCLUDE_DIRS; do
-#  echo "copy"
-#  rsync -r "$INCLUDE_DIR"/* "$ROOT_DIR/Outputs/rclcpp/Includes"
-#done
+colcon build --packages-skip-by-dep python_qt_binding \
+ --build-base "$ROOT_DIR/Builds/rclcpp/Mac" \
+ --merge-install \
+ --catkin-skip-building-tests \
+ --cmake-args \
+ -DCMAKE_POLICY_DEFAULT_CMP0148=OLD \
+ -DCMAKE_CXX_FLAGS="-Wno-error=strict-prototypes -Wno-error=deprecated-copy \
+                    -fexceptions -DPLATFORM_EXCEPTIONS_DISABLED=0 -fmessage-length=0 \
+                    -fpascal-strings -fasm-blocks -ffp-contract=off -isystem /opt/homebrew/include" \
+ -DCMAKE_OSX_ARCHITECTURES="arm64" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.15" \
+ -DBoost_NO_BOOST_CMAKE=TRUE \
+ -DCMAKE_SHARED_LINKER_FLAGS=" -ld_classic" \
+ --no-warn-unused-cli
+
+# Copy the libraries
+DEST="$ROOT_DIR/Outputs/rclcpp"
+find "$ROOT_DIR/Source/rclcpp/install" -name "*.dylib" -exec cp -P {} "$DEST/Libraries/Mac" \;
+
+# Copy the "share" folder
+cp -r -P "$ROOT_DIR/Source/rclcpp/install/share" "$DEST/Libraries/Mac"
+
+# Copy the includes
+INCLUDE_DIRS=$(find "$ROOT_DIR/Source/rclcpp/install/include" -type d -maxdepth 1)
+for INCLUDE_DIR in $INCLUDE_DIRS; do
+  LIBRARY_NAME=$(basename "$INCLUDE_DIR")  
+  if [ -e "$INCLUDE_DIR/$LIBRARY_NAME" ]; then
+    cp -r "$INCLUDE_DIR"/* "$DEST/Includes"
+  else
+    cp -r "$INCLUDE_DIR" "$DEST/Includes"
+  fi
+done
   
 #echo -e "Cleaning up output directory...\n"
 #rm -rf "$ROOT_DIR/Outputs/rclcpp/Libraries/Mac/cmake"
@@ -114,8 +151,8 @@ find "$ROOT_DIR/Source/rclcpp/install" -type d -maxdepth 1 -exec bash -c '[[ -e 
 #rm -f "$ROOT_DIR/Outputs/rclcpp/Libraries/Mac/libutf8_range_lib.a" # Redundant with libutf8_range.a
 
 echo -e "Archiving outputs...\n"
-ARCHIVE="$ROOT_DIR/Releases/TempoThirdParty-rclcpp-Mac-$TAG.tar.gz"
-rm -rf "$ARCHIVE"
-tar -C "$ROOT_DIR/Outputs" -czf "$ARCHIVE" rclcpp
+RCLCPP_ARCHIVE="$ROOT_DIR/Releases/TempoThirdParty-rclcpp-Mac-$TAG.tar.gz"
+rm -rf "$RCLCPP_ARCHIVE"
+tar -C "$ROOT_DIR/Outputs" -czf "$RCLCPP_ARCHIVE" rclcpp
 
-echo "Done! Archive: $ARCHIVE"
+echo "Done! Archives: $RCLCPP_ARCHIVE"
